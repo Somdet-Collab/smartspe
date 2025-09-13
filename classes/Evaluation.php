b246e08be16ff1eeea1f54183a068ba4fa5fa571
Somdet <fluksomdet@gmail.com>
2025-09-13 12:13:56 +0800

    Create Evaluation.php

---
+++ classes/Evaluation.php
@@ -0,0 +1,13 @@
+<?php
+
+class Evaluation
+{
+    private $scores;
+    private $comment;
+
+    public function __construct()
+    {
+        $this->scores = [];
+        $this->comment = [];
+    } 
+}
