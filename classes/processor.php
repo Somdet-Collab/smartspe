e85cde2b19f659c0f304073f4477e366a7a8c3f7
Somdet <fluksomdet@gmail.com>
2025-09-28 17:44:04 +0700

    Update processor.php

--- classes/processor.php
+++ classes/processor.php
@@ -3,8 +3,6 @@
 namespace mod_smartspe;
 
 require 'vendor/autoload.php';
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 use mod_smartspe\db_evaluation as team_manager;
 use mod_smartspe\db_evaluation as evaluation;
@@ -36,6 +34,9 @@ class processor
                 //Upload file
                 move_uploaded_file($tmp_name, $upload_dir.$newfile);
                 echo "<br> The file {$newfile} has been uploaded <br>";
+
+                //Read data to file
+                $this->read_file($upload_dir.$newfile);
             }
             catch (\Exception $e)
             {
@@ -50,7 +51,27 @@ class processor
 
     public function read_file($file_name)
     {
-        
+        $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xls();
+        $spreadsheet = $reader->load($file_name);
+        $worksheets = $spreadsheet->getActiveSheet();
+        $data = $worksheets->toArray();
+
+        //Get index of each specify columns
+        $col_given_name = $this->find_column($data[0], "Given Name");
+        $col_project_title = $this->find_column($data[0], "PROJECT");
+        $col_teamid = $this->find_column($data[0], "TEAM");
+
+        //Save data to database
+        $manager = new team_manager();
+        foreach ($data as $row)
+        {
+            $name = $row[$col_given_name];
+            $project = $row[$col_project_title];
+            $teamid = $row[$col_teamid];
+
+            
+        }
+
     }
 
     public function write_file($filename, $content, $extension="csv")
@@ -58,19 +79,29 @@ class processor
         
     }
 
-    public function find_column($file, $column)
-    {
-        $index = 0;
-
-
-
-        return $index;
-    }
-
     public function save_answers($answers, $userid, $evaluateeid)
     {
 
     }
 
-    
+    public function find_column($row, $column_name)
+    {
+        $index = 0;
+
+        //Loop the header
+        //Key is index
+        foreach($row as $key => $column)
+        {
+            if ($column_name == $column)
+            {
+                $index = $key;
+                break;
+            }
+            else
+            {
+                return $index = -1;
+            }
+        }
+        return $index;
+    }
 }
