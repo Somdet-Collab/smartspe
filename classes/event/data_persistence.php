029e53f0a4f417d6e0ddeec0e0092d1a4ebb8038
Somdet <fluksomdet@gmail.com>
2025-10-13 22:01:05 +0700

    Update data persistence

--- classes/event/data_persistence.php
+++ classes/event/data_persistence.php
@@ -2,10 +2,69 @@
 
 namespace mod_smartspe\classes\event;
 
+use question_engine;
+
 defined('MOODLE_INTERNAL') || die();
 
 class data_persistence
 {
-    
+    protected $attempt;
+
+    public function __contruct($attemptid)
+    {
+        global $DB;
+
+        $this->attempt = $DB->get_record('quiz_attempts', ['id' => $attemptid], '*', MUST_EXIST);
+    }
+    public function load_attempt_questions() 
+    {
+        // Load all questions and their current state
+        $quba = question_engine::load_questions_usage_by_activity($this->attempt->uniqueid);
+
+        //Get all questions
+        $questions = [];
+        foreach ($quba->get_slots() as $slot) 
+        {
+            $qa = $quba->get_question_attempt($slot);
+            $question = $qa->get_question();
+
+            $questions[$slot] = 
+            [
+                'id' => $question->id,
+                'name' => $question->name,
+                'text' => $question->questiontext,
+                'state' => $qa->get_state(),
+                'current_answer' => $qa->get_last_step() ? $qa->get_last_step()->get_data() : null
+            ];
+        }
+
+        return $questions;
+    }
+
+    public function update_attempt_answers($slot, $newdata)
+    {
+        $quba = question_engine::load_questions_usage_by_activity($this->attempt->uniqueid);
+
+        $qa = $quba->get_question_attempt($slot);
+
+        if (!$qa) 
+        {
+            throw new \Exception("Question slot {$slot} not found in this attempt.");
+        }
+
+        // Start a new step for this attempt
+        $qa->start_new_step();
+
+        // Set the new answer
+        $qa->set_response($newdata);
+
+        // Finish the step (not graded yet)
+        $qa->finish_step();
+
+        // Save the question usage (persist changes)
+        $quba->save_all_steps();
+
+        return true;
+    }
 
 }
--- classes/event/questions_handler.php
+++ classes/event/questions_handler.php
@@ -2,7 +2,6 @@
 
 namespace mod_smartspe\classes\event;
 
-use question_engine;
 use question_bank;
 use question_category;
 use context_course;
@@ -49,42 +48,12 @@ class questions_handler
         return $questionid;
     }
 
-    public function load_attempt_questions($attemptid) 
-    {
-        global $DB;
-
-        $attempt = $DB->get_record('quiz_attempts', ['id' => $attemptid], '*', MUST_EXIST);
-
-        // Load all questions and their current state
-        $quba = question_engine::load_questions_usage_by_activity($attempt->uniqueid);
-
-        //Get all questions
-        $questions = [];
-        foreach ($quba->get_slots() as $slot) 
-        {
-            $qa = $quba->get_question_attempt($slot);
-            $question = $qa->get_question();
-
-            $questions[$slot] = 
-            [
-                'id' => $question->id,
-                'name' => $question->name,
-                'text' => $question->questiontext,
-                'state' => $qa->get_state(),
-                'current_answer' => $qa->get_last_step() ? $qa->get_last_step()->get_data() : null
-            ];
-        }
-
-        return $questions;
-
-    }
-
     public function get_all_questions()
     {
         global $DB;
 
         // Get all questions in this category
-        $records = $DB->get_records('question', ['category' => $this->category]);
+        $records = $DB->get_records('question', ['category' => $this->category->id]);
 
         // Format results as array
         $questions = [];
