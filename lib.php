9c67897adca1208afd9f2dc9520661593a5a3c96
Somdet <fluksomdet@gmail.com>
2025-10-19 20:26:46 +0700

    Add mod_form

--- lib.php
+++ lib.php
@@ -1,18 +1,109 @@
 <?php
-
 defined('MOODLE_INTERNAL') || die();
 
-function smartspe_add_instance()
-{
+/**
+ * Library of functions and constants for module smartspe
+ *
+ * @package    mod_smartspe
+ * @copyright  2025 
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
+/**
+ * Add smartspe instance.
+ *
+ * Called when a new instance of the module is created.
+ *
+ * @param stdClass $data An object from the form in mod_form.php
+ * @param mod_smartspe_mod_form $mform
+ * @return int new smartspe instance id
+ */
+function smartspe_add_instance($data, $mform = null) 
+{
+    global $DB;
+
+    // Add created time.
+    $data->timecreated = time();
+
+    // Insert new record into the module table.
+    $id = $DB->insert_record('smartspe', $data);
+
+    // Return the new instance id.
+    return $id;
 }
 
-function smartspe_update_instance()
+/**
+ * Update smartspe instance.
+ *
+ * Called when an existing instance is updated.
+ *
+ * @param stdClass $data An object from the form in mod_form.php
+ * @param smartspe_mod_form $mform
+ * @return bool true on success, false otherwise
+ */
+function smartspe_update_instance($data, $mform = null) 
 {
+    global $DB;
 
+    $data->timemodified = time();
+    $data->id = $data->instance; // Important: set the correct id.
+
+    return $DB->update_record('smartspe', $data);
 }
 
-function smartspe_delete_instance()
+/**
+ * Delete smartspe instance.
+ *
+ * Called when an instance of the module is deleted.
+ *
+ * @param int $id ID of the module instance
+ * @return bool true on success, false otherwise
+ */
+function smartspe_delete_instance($id) 
 {
-    
+    global $DB;
+
+    if (!$record = $DB->get_record('smartspe', ['id' => $id])) 
+    {
+        return false;
+    }
+
+    // Delete dependent records first if any (like evaluations, teams, etc.)
+    // Example:
+    // $DB->delete_records('smartspe_evaluations', ['smartspeid' => $id]);
+    // $DB->delete_records('smartspe_teams', ['smartspeid' => $id]);
+
+    // Delete main instance.
+    $DB->delete_records('smartspe', ['id' => $id]);
+
+    return true;
+}
+
+/**
+ * Supports feature detection.
+ *
+ * @param string $feature FEATURE_xx constant for requested feature
+ * @return mixed true if module supports feature, null if unknown
+ */
+function smartspe_supports($feature) 
+{
+    switch ($feature) 
+    {
+        case FEATURE_MOD_INTRO:          return true;
+        case FEATURE_SHOW_DESCRIPTION:   return true;
+        case FEATURE_BACKUP_MOODLE2:     return true;
+        default:                         return null;
+    }
+}
+
+/**
+ * Extend settings navigation with smartspe specific settings.
+ *
+ * @param settings_navigation $settingsnav
+ * @param navigation_node $smartspe_node
+ */
+function smartspe_extend_settings_navigation($settingsnav, $smartspe_node) 
+{
+    // Example: add a custom link to your plugin page.
+    // $url = new moodle_url('/mod/smartspe/custom.php', ['id' => $smartspe_node->id]);
 }
---
+++ mod_form.php
@@ -0,0 +1,76 @@
+<?php
+
+use mod_smartspe\classes\event\duration_controller;
+use core\exception\moodle_exception;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/course/moodleform_mod.php');
+
+/**
+ * Form for creating or editing a SmartSpe activity
+ *
+ * @package    mod_smartspe
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class smartspe_mod_form extends moodleform_mod
+{
+
+    public function definition() 
+    {
+        global $CFG, $COURSE;
+
+        $mform = $this->_form;
+
+        // Activity name.
+        $mform->addElement('text', 'name', get_string('smartspe_name', 'mod_smartspe'), ['size' => '64']);
+        $mform->setType('name', PARAM_TEXT);
+        $mform->addRule('name', null, 'required', null, 'client');
+        $mform->addHelpButton('name', 'smartspe_name', 'mod_smartspe');
+
+        // Intro / description.
+        $this->standard_intro_elements(get_string('smartspe_intro', 'mod_smartspe'));
+
+        // --- Submission period section ---
+        $mform->addElement('header', 'timinghdr', get_string('submissionperiod', 'mod_smartspe'));
+
+        // Start date.
+        $mform->addElement('date_time_selector', 'startdate', get_string('submissionstart', 'mod_smartspe'), ['optional' => false]);
+        $mform->setDefault('startdate', time());
+
+        // End date (deadline).
+        $mform->addElement('date_time_selector', 'enddate', get_string('submissionend', 'mod_smartspe'), ['optional' => false]);
+        $mform->setDefault('enddate', time() + 7 * 24 * 60 * 60); // default 1 week later
+
+        // Standard course module elements.
+        $this->standard_coursemodule_elements();
+
+        // Add standard buttons, common to all modules.
+        $this->add_action_buttons();
+    }
+
+    /**
+     * Extra validation to ensure start date < end date.
+     */
+    public function validation($data, $files) 
+    {
+        $errors = parent::validation($data, $files);
+
+        // Check using your duration_controller.
+        try 
+        {
+            $duration = new duration_controller
+            (
+                $data['startdate'],
+                $data['enddate']
+            );
+        } 
+        catch (moodle_exception $e) 
+        {
+            $errors['startdate'] = $e->getMessage();
+            $errors['enddate'] = $e->getMessage();
+        }
+
+        return $errors;
+    }
+}
