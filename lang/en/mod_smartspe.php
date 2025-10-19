52306d72a3041f573a2f6c90e28a1b2bcc100ecb
Somdet <fluksomdet@gmail.com>
2025-10-19 20:34:57 +0700

    Add lang/en

---
+++ lang/en/mod_smartspe.php
@@ -0,0 +1,21 @@
+<?php
+// Standard Moodle language file for SmartSpe module.
+
+$string['pluginname'] = 'Self Peer Evaluation';
+$string['modulename'] = 'Self Peer Evaluation';
+$string['modulenameplural'] = 'Self Peer Evaluations';
+
+$string['smartspe_name'] = 'Evaluation Name';           // Form field for activity name
+$string['smartspe_name_help'] = 'Enter a descriptive name for this evaluation activity.';
+
+$string['submissionperiod'] = 'Submission Period';      // Header in mod_form.php
+$string['submissionstart'] = 'Submission Start Date';   // Start date label
+$string['submissionend'] = 'Submission End Date';       // End date label
+$string['submissionwindow'] = 'Submission Window';      // Display to students
+$string['submissionopen'] = 'Submission is open';
+$string['submissionclosed'] = 'Submission is closed';
+
+$string['pluginadministration'] = 'Self Peer Evaluation administration'; // Admin menu
+$string['privacy:metadata'] = 'The Self Peer Evaluation plugin stores user submissions and peer evaluation data.';
+
+?>
