df2bc0af29aba0817ebe44a287c06e98bd7d19c7
Somdet <fluksomdet@gmail.com>
2025-10-24 00:01:11 +0700

    Edit attemptid

--- classes/handler/submission_handler.php
+++ classes/handler/submission_handler.php
@@ -12,11 +12,13 @@ class submission_handler
 {
     protected $evaluator;
     protected $courseid;
+    protected $attemptid;
 
-    public function __construct($evaluator, $courseid)
+    public function __construct($evaluator, $courseid, $attemptid)
     {
         $this->evaluator = $evaluator;
         $this->courseid = $courseid;
+        $this->attemptid = $attemptid;
     }
 
     /**
@@ -45,7 +47,7 @@ class submission_handler
             //Save evaluation info into database
             //Return true if data are saved in db successfully
             return $evaluation->save_answers_db($answers, $userid, 
-                                            $evaluateeid, $this->courseid, $comment, $self_comment);
+                                            $evaluateeid, $this->courseid, $comment, $this->attemptid, $self_comment);
         }
         else
         {
