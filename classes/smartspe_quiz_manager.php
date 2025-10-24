be945daa9cacdf350a37b23182ddec41034a5510
Somdet <fluksomdet@gmail.com>
2025-10-25 00:38:48 +0700

    Update smartspe_quiz_manager.php

--- classes/smartspe_quiz_manager.php
+++ classes/smartspe_quiz_manager.php
@@ -185,9 +185,16 @@ class smartspe_quiz_manager
             [$questions, $comments] = $data_persistence->load_attempt_questions();
 
             //Get all saved answers
-            foreach($data_persistence->get_slots() as $slot)
-                foreach($questions[$slot]['current_answer'] as $key => $answer)
-                    $answers[] = $answer;
+            foreach ($data_persistence->get_slots() as $slot) 
+            {
+                //Ensure it is in correct format array[1,2,3,4]
+                $current = $questions[$slot]['current_answer'];
+                if (is_array($current)) {
+                    $answers[] = (int) reset($current);
+                } else {
+                    $answers[] = (int) $current;
+                }
+            }
 
             //Get comment
             if($comments)
