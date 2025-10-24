4e3aa1b8e1a286bc8b0f22ceb16f9aa9027e416c
Somdet <fluksomdet@gmail.com>
2025-10-24 16:01:58 +0800

    Add sentiment analysis

---
+++ classes/handler/sentiment_analysis_handler.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace mod_smartspe\handler;
+
+use mod_smartspe\db_evaluation;
+use core\exception\moodle_exception;
+
+defined('MOODLE_INTERNAL') || die();
+
+//This class carries sentiment analysis process
+
+class sentiment_analysis_handler
+{
+    public function process_sentiment_analysis($evaluationid)
+    {
+        global $DB;
+        //collect records all records
+        $record = $DB->get_record('smartspe_evaluation', ['evaluationid' => $evaluationid]);
+        $comment = $record->comment;
+        $self_comment = $record->self_comment;
+
+        //crete AI model
+
+        //Feed comment and self comment into AI
+
+        //Get AI result
+        $polarity = "Positive";
+        $score = "4.5";
+
+        //Save polarity and score
+        $db_manager = new db_evaluation();
+        $sentimentid = $db_manager->save_sentiment_analysis($evaluationid, $polarity, $score);
+
+        return $sentimentid;
+    }
+}
