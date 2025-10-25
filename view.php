13be5b177c313dac5e2fa112c009d6e66c3b3392
Somdet <fluksomdet@gmail.com>
2025-10-25 15:32:27 +0700

    Update view.php

--- view.php
+++ view.php
@@ -67,11 +67,12 @@ foreach ($members as $memberid)
 
     foreach ($questions as $question) 
     {
-        echo "Question for $member_name: $question <br>";
-        if ($question->qtype === 'multichoice' && $mcq_count < 5) {
+        $qtext = $question['text'];
+        echo "Question for $member_name: $qtext <br>";
+        if ($question['qtype'] === 'multichoice' && $mcq_count < 5) {
             $answers[] = rand(1, 3); // simulate MCQ answer
             $mcq_count++;
-        } elseif ($question->qtype === 'essay' && $comment_count < 1) {
+        } elseif ($question['qtype'] === 'essay' && $comment_count < 1) {
             $comment = "Peer comment for member $memberid";
             $comment_count++;
         }
