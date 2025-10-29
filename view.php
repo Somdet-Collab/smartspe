94c77b83fa408ab426447f8e4a03134b1fd3b0f8
cellWall <salwasanaullahkhan@gmail.com>
2025-10-29 13:09:53 +0800

    cleaned up code
    removed redundancies

--- view.php
+++ view.php
@@ -7,32 +7,27 @@ use core\exception\moodle_exception;
 
 global $DB, $USER, $PAGE;
 
-// --- 1. Get basic parameters ---
-$id = required_param('id', PARAM_INT); // Course module ID
-
-$cm = get_coursemodule_from_id('smartspe', $id, 0, false, MUST_EXIST);
+// get basic parameters
+$id = required_param('id', PARAM_INT);
+$cm = get_coursemodule_from_id('smartspe', $id, 0, false, MUST_EXIST); 
 $course = $DB->get_record('course', ['id' => $cm->course], '*', MUST_EXIST);
 $context = \context_module::instance($cm->id);
 $instance = $DB->get_record('smartspe', ['course' => $course->id], '*', MUST_EXIST);
 $instanceid = $instance->id;
 require_login($course, true, $cm);
 
-// --- 2. Set up the page ---
+// set up the page
 $PAGE->set_url('/mod/smartspe/view.php', ['id' => $id]);
 $PAGE->set_title(get_string('pluginname', 'mod_smartspe'));
 $PAGE->set_heading($course->fullname);
 $PAGE->set_context($context);
 $PAGE->set_pagelayout('incourse');
 
-// --- 3. Load activity instance ---
-$smartspe = $DB->get_record('smartspe', ['id' => $cm->instance], '*', MUST_EXIST);
-$instanceid = $smartspe->id;
-
-// --- 4. Determine user role ---
+// determine user role
 $is_teacher = has_capability('mod/smartspe:manage', $context);
 $is_student = !$is_teacher && has_capability('mod/smartspe:submit', $context);
 
-// --- 6. Get renderer ---
+// get renderer
 $output = $PAGE->get_renderer('mod_smartspe');
 
 // output starts here
@@ -41,20 +36,17 @@ echo $OUTPUT->header();
 $quiz_manager = null;
 if ($is_student) 
 {
-    $quiz_manager = new \mod_smartspe\smartspe_quiz_manager($USER->id, $course->id, $context, $instanceid, $cm->id);
+    $quiz_manager = new mod_smartspe\smartspe_quiz_manager($USER->id, $course->id, $context, $instanceid, $cm->id);
     echo $output->render(new \mod_smartspe\output\student_view($quiz_manager));
 } 
 else if ($is_teacher)
 {
     try {
-        $quiz_manager = new \mod_smartspe\smartspe_quiz_manager(
-            $USER->id, $course->id, $context, $instanceid, $cm->id
-        );
+        $quiz_manager = new mod_smartspe\smartspe_quiz_manager($USER->id, $course->id, $context, $instanceid, $cm->id);
     } catch (Exception $e) {
         echo "Quiz manager creation failed: " . $e->getMessage();
         die();
     }
-
     echo $output->render(new \mod_smartspe\output\teacher_view($quiz_manager));
 }
 
