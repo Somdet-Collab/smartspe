e45c0d57cd1d4196e6ed4529767c33704863fcd1
Somdet <fluksomdet@gmail.com>
2025-11-02 19:10:41 +0700

    Update observer.php

--- classes/observer.php
+++ classes/observer.php
@@ -32,22 +32,4 @@ class observer {
         $notifier->noti_eval_submitted($event->userid);
     }
 
-    /**
-     * Handle download event.
-     *
-     * @param \mod_smartspe\event\file_download $event
-     */
-    public static function file_download(\mod_smartspe\event\file_download $event) 
-    {
-        debugging("Download triggered by user {$event->userid} for cmid {$event->contextinstanceid}", DEBUG_DEVELOPER);
-
-        // Define default filename
-        $filename = 'smartspe_report_' . time();
-
-        //Trigger download
-        $downloader = new \mod_smartspe\handler\download_handler();
-        $downloader->download_file($filename, "csv", $event->other['course']);
-
-    }
-
 }
